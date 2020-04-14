0a9bde9a265509059008d4dee58a57a3b9e773ea
woshiwwy16@126.com <Wwy-+901116>
2020-04-14 12:45:36 +0800

    modified custom view

--- app/src/main/java/com/rick/jetpackexample/view/SimpleImageButton.java
+++ app/src/main/java/com/rick/jetpackexample/view/SimpleImageButton.java
@@ -1,6 +1,109 @@
 package com.rick.jetpackexample.view;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.databinding.DataBindingUtil;
+
+import com.rick.jetpackexample.R;
+import com.rick.jetpackexample.databinding.SimpleImageButtonBinding;
 
 public class SimpleImageButton extends ConstraintLayout {
+
+    private Context mContext;
+
+    private SimpleImageButtonBinding mBind;
+
+    private ImageView imageView;
+
+    private TextView textView;
+
+    private Paint mPaint;
+
+    private int backgroundColor;
+
+    private int textColor;
+
+    private String text;
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    @Override
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public SimpleImageButton(Context context) {
+        super(context);
+        setWillNotDraw(false);
+        this.mContext = context;
+        initView();
+    }
+
+    public SimpleImageButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWillNotDraw(false);
+        this.mContext = context;
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SimpleImageButton);
+        backgroundColor = typedArray.getColor(R.styleable.SimpleImageButton_background_color, Color.argb(255, 222, 212, 247));
+        text = typedArray.getText(R.styleable.SimpleImageButton_text).toString();
+        textColor = typedArray.getColor(R.styleable.SimpleImageButton_text_color, Color.argb(255, 117, 64, 238));
+        typedArray.recycle();
+        initView();
+    }
+
+    private void initView() {
+        mBind = DataBindingUtil.inflate(LayoutInflater.from(mContext), R.layout.simple_image_button, this, true);
+        //LayoutInflater.from(mContext).inflate(R.layout.simple_image_button, this);
+        //textView = findViewById(R.id.tv_text);
+        //imageView = findViewById(R.id.iv_img);
+        mBind.tvText.setText(text);
+        mBind.tvText.setTextColor(textColor);
+        //textView.setText(text);
+        //textView.setTextColor(textColor);
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(backgroundColor);
+        mPaint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // 画布的原点在坐上角（0，0）处
+        float right = getWidth();
+        float bottom = getHeight();
+
+        canvas.drawRoundRect(0, 0, right, bottom, 5, 5, mPaint);
+    }
 }
--- app/src/main/java/com/rick/jetpackexample/view/SimpleView.java
+++ app/src/main/java/com/rick/jetpackexample/view/SimpleView.java
@@ -1,6 +1,86 @@
 package com.rick.jetpackexample.view;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.text.Layout;
+import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.rick.jetpackexample.R;
 
 public class SimpleView extends View {
+
+    private Context context;
+
+    private int backgroundColor;
+
+    private int mWidth = 200;
+
+    private int mHeight = 200;
+
+    private Paint mPaint;
+
+    private Paint shadowPaint;
+
+    private Bitmap bitmap;
+
+    public SimpleView(Context context) {
+        super(context);
+        initPaint();
+    }
+
+    public SimpleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SimpleImageButton);
+        backgroundColor = typedArray.getColor(R.styleable.SimpleImageButton_background_color, Color.argb(255, 0, 0, 0));
+        typedArray.recycle();
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(backgroundColor);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(5.0f);
+
+        shadowPaint = new Paint();
+        shadowPaint.setShadowLayer(20, 20, 20, backgroundColor);
+        bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.argb(255, 153, 153, 153));
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (getLayoutParams().width == ViewGroup.LayoutParams.WRAP_CONTENT && getLayoutParams().height == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            setMeasuredDimension(mWidth, mHeight);
+        } else if (getLayoutParams().width == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            setMeasuredDimension(mWidth, heightSize);
+        } else if (getLayoutParams().height == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            setMeasuredDimension(widthSize, mHeight);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        canvas.drawRoundRect(0, 0, width, height, 10, 10, mPaint);
+    }
 }
